780af990afdf9485064470515b95de23c506cb31
nmolanog <nmolanog@gmail.com>
2020-03-07 09:48:50 -0500

    Added generation of xlsx file to change names in each data set

--- R/descriptive.R
+++ R/descriptive.R
@@ -22,6 +22,7 @@ load(paste0(path_RData,"/", "acroos_dep",".RData"))
 data_list<-list(z0_cl,z0_F1,z0_F2,z0_ctr)
 names(data_list)<-c("cl","F1","F2","ctr")
 wb <- openxlsx::createWorkbook()
+wb2<- openxlsx::createWorkbook()
 for(i in seq_along(data_list)){
   z0<-data_list[[i]]
   
@@ -51,6 +52,11 @@ for(i in seq_along(data_list)){
   openxlsx::writeData(wb,x=dscat_temp , sheet = nameshetb,colNames =F)
   borders_for_dscr1.cat(wb,sheet_name=nameshetb,dscat_temp,borderStyle="thick")
   
+  openxlsx::addWorksheet(wb2, sheetName = names(data_list)[i])
+  openxlsx::freezePane(wb2, sheet = names(data_list)[i], firstActiveRow = 1, firstActiveCol = 2)
+  openxlsx::writeData(wb2,x=data.frame(var=colnames(z0),new_names=NA) , sheet = names(data_list)[i],colNames =T)
+  
 }
 openxlsx::saveWorkbook(wb,paste0(output_path,"/","descriptive_v0.xlsx"),TRUE)
+openxlsx::saveWorkbook(wb2,paste0(output_path,"/","new_names.xlsx"),TRUE)
 
